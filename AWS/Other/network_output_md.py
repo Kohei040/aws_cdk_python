b88f7f49333a23b8957086d9257d4d8ea91ef9cb
Kohei040 <i_kohei0316@icloud.com>
2019-07-12 12:58:19 +0900

    Update Subnet Output

--- AWS/Other/network_output_md.py
+++ AWS/Other/network_output_md.py
@@ -2,27 +2,26 @@
 import boto3
 
 file = 'network.md'
-
 client = boto3.client('ec2')
 
 vpc_list = client.describe_vpcs()['Vpcs']
 vpc_ids, vpc_cider_blocks, vpc_names= [], [], []
 
 def main():
-    vpc_contents()
-    vpc_main()
-    subnet_main()
+    get_vpc()
+    vpc_output()
+    subnet_output()
     with open(file, 'r') as f:
         print(f.read())
 
-def vpc_contents():
+def get_vpc():
     for i, vpc in enumerate(vpc_list):
         vpc_ids.append(vpc['VpcId'])
         vpc_cider_blocks.append(vpc['CidrBlock'])
         vpc_names.append(vpc_check_tag_name(vpc, vpc_ids[i]))
     return 0
 
-def vpc_main():
+def vpc_output():
     with open(file, 'w', encoding='utf-8') as f:
         f.write('## VPC\n\n| Name | VPC ID | IPv4 CIDR |\n|:--|:--|:--|')
         for i, vpc_id in enumerate(vpc_ids):
@@ -42,12 +41,13 @@ def vpc_check_tag_name(vpc, id):
             tag_name = ' '
             return tag_name
 
-def subnet_main():
+def subnet_output():
     with open(file, 'a') as f:
-        f.write('\n\n## Subnet\n\n')
+        f.write('\n\n## Subnet')
     for i, vpc_id in enumerate(vpc_ids):
         with open(file, 'a') as f:
             f.write('\n\n#### ' + vpc_names[i] + '(' + vpc_id + ')')
+            f.write('\n\n| Name | Subnet ID | IPv4 CIDR | AZ |\n|:--|:--|:--|:--|')
             vpc_subnet_list = client.describe_subnets(
                 Filters=[
                     {
@@ -57,8 +57,16 @@ def subnet_main():
                         ]
                     },
                 ],
-            )['Subnets'][i]['SubnetId']
-            print(vpc_subnet_list)
+            )['Subnets']
+            for i, subnet in enumerate(vpc_subnet_list):
+                subnet_id = subnet['SubnetId']
+                subnet_az = subnet['AvailabilityZone']
+                subnet_cider = subnet['CidrBlock']
+                try:
+                    subnet_name = subnet['Tags'][0]['Value']
+                except KeyError:
+                    subnet_name = ' '
+                f.write('\n|' + subnet_name + '|' + subnet_id + '|' + subnet_cider + '|' + subnet_az + '|')
     return 0
 
 if __name__ == '__main__':
